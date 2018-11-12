51decb75890c8968a71fdeae41f5059e87165a6b
condini-mastheus <matheus.condini@gmail.com>
2018-11-12 21:19:36 -0200

    Initial commit

---
+++ src/pages/Login.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+// Styles
+import "./Login.css";
+import twitterLogo from "../twitter.svg";
+
+export default class Login extends Component {
+  state = {
+    username: ""
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { username } = this.state;
+
+    if (!username.length) return;
+
+    localStorage.setItem("@GoTwitter:username", username);
+
+    this.props.history.push("/timeline");
+  };
+
+  handleInputChange = e => {
+    this.setState({ username: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="login-wrapper">
+        <img src={twitterLogo} alt={"GoTwitter"} />
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.username}
+            placeholder="Nome de usuário"
+            onChange={this.handleInputChange}
+          />
+
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
+  }
+}
